5fce8c52da072a22b05fd6c4d8fc86b5507bdcb8
Emma <emma.rouquet7@gmail.com>
2019-11-30 17:17:48 +0100

    modification js

---
+++ feurouge/index.js
@@ -0,0 +1,6 @@
+document.getElementById("monElement").className = "maClasse";
+
+// Ou plusieurs classes, séparée par un espace.
+
+document.getElementById("monElement").className = "maClasse1 maClasse2";
+
