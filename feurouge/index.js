98ed994c7d10311b63b25144a92995e1900b8dd5
Emma <emma.rouquet7@gmail.com>
2019-12-03 09:38:10 +0100

    Feu rouge fonctionne

--- feurouge/index.js
+++ feurouge/index.js
@@ -1,6 +1,31 @@
-document.getElementById("monElement").className = "maClasse";
+var signState = 0;
+var fireRed = document.getElementsByClassName("off-fire-r")[0];
+var fireYellow = document.getElementsByClassName("off-fire-y")[0];
+var fireGreen = document.getElementsByClassName("off-fire-g")[0];
 
-// Ou plusieurs classes, séparée par un espace.
+function setSignRed() {
+    fireRed.className = "off-fire-r fire-red";
+    fireGreen.className = "off-fire-g";
+    fireYellow.className = "off-fire-y";
+    setTimeout(function() { setSignGreen(); }, 7000);
+}
 
-document.getElementById("monElement").className = "maClasse1 maClasse2";
+function setSignYellow() {
+    fireRed.className = "off-fire-r";
+    fireGreen.className = "off-fire-g";
+    fireYellow.className = "off-fire-y fire-yellow";
+    setTimeout(function() { setSignRed(); }, 3000);
+}
 
+function setSignGreen() {
+    fireRed.className = "off-fire-r";
+    fireGreen.className = "off-fire-g fire-green";
+    fireYellow.className = "off-fire-y";
+    setTimeout(function() { setSignYellow(); }, 7000);
+}
+
+function startSign() {
+    setSignRed();
+}
+
+startSign()
