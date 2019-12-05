66204a8718f557806f3d3b2942920ecfd6c50cb9
Emma <emma.rouquet7@gmail.com>
2019-12-05 08:59:48 +0100

    finition feu

--- feurouge/index.js
+++ feurouge/index.js
@@ -1,12 +1,13 @@
-var signState = 0;
 var fireRed = document.getElementsByClassName("off-fire-r")[0];
 var fireYellow = document.getElementsByClassName("off-fire-y")[0];
 var fireGreen = document.getElementsByClassName("off-fire-g")[0];
+var yellowTimer = null; 
 
 function setSignRed() {
     fireRed.className = "off-fire-r fire-red";
     fireGreen.className = "off-fire-g";
     fireYellow.className = "off-fire-y";
+    yellowTimer = null;
     setTimeout(function() { setSignGreen(); }, 7000);
 }
 
@@ -14,14 +15,23 @@ function setSignYellow() {
     fireRed.className = "off-fire-r";
     fireGreen.className = "off-fire-g";
     fireYellow.className = "off-fire-y fire-yellow";
-    setTimeout(function() { setSignRed(); }, 3000);
+    yellowTimer = null;
+    setTimeout(function() { setSignRed(); }, 2500);
 }
 
 function setSignGreen() {
     fireRed.className = "off-fire-r";
     fireGreen.className = "off-fire-g fire-green";
     fireYellow.className = "off-fire-y";
-    setTimeout(function() { setSignYellow(); }, 7000);
+    yellowTimer = setTimeout(function() { setSignYellow(); }, 7000);
+}
+
+function buttonClick() {
+    if (yellowTimer === null) {
+        return false;
+    }
+    clearTimeout(yellowTimer);
+    yellowTimer = setTimeout(function() { setSignYellow(); }, 2000);
 }
 
 function startSign() {
